669f600ad2f227a198821c68b0edf30d12f9b38f
codeAnyeliRodriguez <anyelib.dev@gmail.com>
2023-07-27 15:23:29 -0400

    bibliotecadev

---
+++ javascript/curso javascript DOM/toppings-de-pizza/app.js
@@ -0,0 +1,7 @@
+const titulo = document.getElementById('titulo');
+// console.log(typeof contenedor.innerHTML);
+// console.log(typeof titulo.innerText);
+// console.log(titulo.tagName);
+// console.log(titulo);
+const toppings = document.getElementsByClassName('topping');
+console.log(toppings[0].id);
